a3237c6e8783b648b6a73744e849f1f1a69666f5
sonat <enginsonat86@hotmail.com>
2015-12-09 10:32:23 +0200

    Hibernate

--- src/main/java/com/sonat/blog/dao/UserDaoImpl.java
+++ src/main/java/com/sonat/blog/dao/UserDaoImpl.java
@@ -1,5 +1,35 @@
 package com.sonat.blog.dao;
 
-public class UserDaoImpl {
+import java.util.ArrayList;
+import java.util.List;
+import com.sonat.blog.util.HibernateUtil;
+import com.sonat.blog.domain.User;
 
+public class UserDaoImpl implements UserDao {
+
+//	private SessionFactory sessionFactory;
+
+	@SuppressWarnings("unchecked")
+	public User findByUserName(String username) {
+
+		List<User> users = new ArrayList<User>();
+
+		users = HibernateUtil.getSessionFactory().getCurrentSession().createQuery("from User where username=?")
+				.setParameter(0, username).list();
+
+		if (users.size() > 0) {
+			return users.get(0);
+		} else {
+			return null;
+		}
+
+	}
+//
+//	public SessionFactory getSessionFactory() {
+//		return sessionFactory;
+//	}
+//
+//	public void setSessionFactory(SessionFactory sessionFactory) {
+//		this.sessionFactory = sessionFactory;
+//	}
 }
