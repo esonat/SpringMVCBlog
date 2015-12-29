076419d2bd60f0bef267d3871dbd95651cb629fe
sonat <enginsonat86@hotmail.com>
2015-12-29 17:01:09 +0200

    Added CommentDaoHibernate tests

--- src/test/java/com/sonat/blog/dao/hibernate/testCommentDaoHibernate.java
+++ src/test/java/com/sonat/blog/dao/hibernate/testCommentDaoHibernate.java
@@ -1,7 +1,5 @@
 package com.sonat.blog.dao.hibernate;
 
-import org.apache.taglibs.standard.lang.jstl.test.StaticFunctionTests;
-import org.hibernate.stat.SecondLevelCacheStatistics;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 import com.sonat.blog.dao.CommentDao;
 import com.sonat.blog.dao.CategoryDao;
 import com.sonat.blog.dao.PostDao;
@@ -16,8 +15,8 @@ import com.sonat.blog.dao.UserDao;
 import com.sonat.blog.domain.Category;
 import com.sonat.blog.domain.Post;
 import com.sonat.blog.domain.User;
-
 import junit.framework.Assert;
+import net.sf.ehcache.terracotta.TerracottaBootstrapCacheLoader;
 
 import com.sonat.blog.domain.Comment;
 
@@ -25,6 +24,7 @@ import com.sonat.blog.domain.Comment;
 @ContextConfiguration(locations={"classpath:/META-INF/spring/spring-master.xml",
 								 "classpath:/META-INF/spring/spring-datasource.xml",
 								 "classpath:/META-INF/spring/spring-hibernate.xml"})
+@Transactional  
 public class testCommentDaoHibernate {
 
 	@Autowired
@@ -40,7 +40,8 @@ public class testCommentDaoHibernate {
 	private User user;
 	private Category category;
 	private Post post;
-		
+	private static int totalcount=0;
+	
 	private static final int INVALID_COMMENT_ID=1000;
 	
 	@Ignore
@@ -58,7 +59,23 @@ public class testCommentDaoHibernate {
 				0:commentDao.getPostComments(post.getID()).size();
 	}
 	@Ignore
-	public 
+	public int getCommentCount(){
+		if(commentDao.getAll()==null) return 0;
+		return commentDao.getAll().size();
+	}
+	@Ignore
+	public Comment getValidPostComment(Post post){
+		if(post==null) return null;
+		if(commentDao.getPostComments(post.getID())==null) return null;
+		
+		return commentDao.getPostComments(post.getID()).get(0);
+	}
+	@Ignore
+	public Comment getValidComment(){
+		if(commentDao.getAll()==null) return null;
+		
+		return commentDao.getAll().get(0);
+	}
 	
 	@Test
 	public void testValidAddPostComment(){
@@ -69,9 +86,12 @@ public class testCommentDaoHibernate {
 		comment.setText("Test Comment");
 				
 		commentDao.addPostComment(post, comment);
+		totalcount--;
+		
 		int newcount=getPostCommentCount(post);
 		Assert.assertEquals(oldcount+1,newcount);
 	}
+	
 	@Test(expected=RuntimeException.class)
 	public void testAddNullPostComment(){
 		Post post=null;
@@ -81,6 +101,7 @@ public class testCommentDaoHibernate {
 				
 		commentDao.addPostComment(post, comment);
 	}	
+	
 	@Test(expected=DataAccessException.class)
 	public void testAddInvalidPostComment(){
 		Post post=getValidPost();
@@ -88,11 +109,45 @@ public class testCommentDaoHibernate {
 		Comment comment=new Comment();
 		commentDao.addPostComment(post, comment);
 	}	
+	
 	@Test
 	public void testAddValidChildComment(){
+		Post post		=getValidPost();
+		Comment comment	=getValidPostComment(post);
 		
+		Comment child=new Comment();
+		child.setText("Test Child Comment");
+		
+		
+		int oldcount=getPostCommentCount(post);
+		commentDao.addChildComment(comment, child);
+		
+		totalcount--;
+		
+		int newcount=getPostCommentCount(post);
+		Assert.assertEquals(newcount,oldcount+1);
+	}
+	@Test(expected=DataAccessException.class)
+	public void testAddInvalidChildComment(){
+		Post post		=getValidPost();
+		Comment comment	=getValidPostComment(post);
+		
+		Comment child=new Comment();
+		commentDao.addChildComment(comment, child);
 	}
 	
-	
-	
+	@Test 
+	public void testDeleteValidComment()
+	{
+		Comment comment=getValidComment();
+		Post post=comment.getPost();
+		
+		int oldtotal=getCommentCount();
+		totalcount+=oldtotal;
+		
+		commentDao.delete(comment);
+		int newtotal=totalcount;
+		
+		Assert.assertEquals(newtotal,oldtotal-1);
+		}
 }
